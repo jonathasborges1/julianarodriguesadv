b53c25e88aac2fcadd167890350b0ea9ac056410
Jonathas Borges <jbc@icomp.ufam.edu.br>
2025-03-19 19:20:00 -0400

    deploy #1
    - Adicionado as secrest no github
    - criacao do .yml

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // Necessário para Cloudflare Pages
+  distDir: "out", // Define o diretório de saída para "out"
+  trailingSlash: true, // Adiciona barra no final das URLs, compatível com export estático
+  images: {
+    unoptimized: true, // Evita otimização de imagens no Cloudflare Pages
+  },
 };
 
 export default nextConfig;
